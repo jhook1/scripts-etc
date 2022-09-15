626b8371a29a09f4bf8f22c68cd692b93c1538a6
John Hook <jhook@mcati.com>
2022-09-15 11:19:05 -0400

    Fixed issues with ps bypass script. Tested

--- bypass-ps-policy.bat
+++ bypass-ps-policy.bat
@@ -2,4 +2,4 @@
 :: Lifetime of change is lifetime of spawned Powershell process (I think). Applies to ALL Powershell instances (seemingly; i.e. VS Nuget console).
 :: Must be run as admin (probably).
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\PowerShell" /v EnableScripts /t REG_DWORD /d 1
-powershell.exe -ExectuionPolicy Bypass -Command "& {Get-ExectuionPolicy -List}"
+powershell.exe -NoExit -ExecutionPolicy Bypass -Command "Get-ExecutionPolicy -List"
